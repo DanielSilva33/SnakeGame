b9769b1991be099d8427f2ead9cd0f6cc276126b
Daniel Silva <daniel.oliveiradasilvam@gmail.com>
2020-04-20 20:52:38 -0300

    Snake Game

--- Game/script.js
+++ Game/script.js
@@ -1,88 +1,230 @@
 
-window.onload = function () {
+class createSnake {
+    constructor(gameContext, gameDiv, gameBack) {
+        this.gameContext = gameContext;
+        this.gameDiv = gameDiv;
+        this.gameBack = gameBack;
+        this.snakeX = 2;
+        this.snakeY = this.snakeX;
+        this.vel = 1;
+        this.velX = this.vel;
+        this.velY = 0;
+        this.permision = false;
+        this.snakeTrail = [{ "x": this.snakeX, "y": this.snakeY }];
+    }
 
-    var stage = document.getElementById('stage');
-    var ctx = stage.getContext("2d");
-    document.addEventListener("keydown", keyPush) // evento que chama as teclas do jogo
+    setTrail() {
+        let i = this.snakeTrail.length - 1;
 
-    //controla a velocidade do jogo
-    setInterval(game, 60);
-
-    //constante de velocidade de inicio;
-    const vel = 1;
-
-    //variaveis para definir localização da Snake;
-    var vx = vy = 0;
-    var px = 10;
-    var py = 15;
-    var tp = 20;
-    var qp = 30;
-    var ax = ay = 15;
-
-    var trail = [];
-    tail = 1;
-
-    function game() {
-        px += vx;
-        py += vy;
-
-        if (px < 0) {
-            px = qp - 1
-        }
-        if (px > qp - 1) {
-            px = 0
-        }
-        if (py < 0) {
-            py = qp - 1
-        }
-        if (py > qp - 1) {
-            py = 0
-        }
-
-        ctx.fillStyle = "Cyan";
-        ctx.fillRect(0, 0, stage.width, stage.height);
-
-        ctx.fillStyle = "red";
-        ctx.fillRect(ax * tp, ay * tp, tp, tp);
-
-        ctx.fillStyle = "Orange";
-        for (var i = 0; i < trail.length; i++) {
-            ctx.fillRect(trail[i].x * tp, trail[i].y * tp, tp - 1, tp - 1);
-            if (trail[i].x == px && trail[i].y == py) {
-                vx = vy = 0;
-                tail = 1;
+        for (i; i >= 0; i--) {
+            if (i != 0) {
+                this.snakeTrail[i] = this.snakeTrail[i - 1];
+            } else {
+                this.snakeTrail[0] = { "x": this.snakeX, "y": this.snakeY };
             }
         }
 
-        trail.push({ x: px, y: py });
-        while (trail.length > tail) {
-            trail.shift();
-        }
+    }
 
-        //almentando a cobra conforme vc come as frutas
-        if (ax == px && ay == py) {
-            tail++;
-            ax = Math.floor(Math.random() * qp);
-            ay = Math.floor(Math.random() * qp);
-        }
-    };
-    //função de direção da cobra 
-    function keyPush(event) {
-        if (event.keyCode == 37){ //left
-            vx = -vel;
-            vy = 0
-        }
-        else if(event.keyCode == 38){ // up
-            vx = 0;
-            vy = -vel;
-        }
-        else if(event.keyCode == 39){ // right
-            vx = vel;
-            vy = 0;
-        }
-        else if(event.keyCode == 40){ // down
-            vx = 0;
-            vy = vel;
+    clearSnake() {
+        while (this.snakeTrail.length > 1) {
+            this.snakeTrail.pop();
         }
     }
+
+    lose() {
+
+        for (let i = 0; i < this.snakeTrail.length; i++) {
+
+            if (i != 0 && this.snakeTrail[0].x == this.snakeTrail[i].x &&
+                this.snakeTrail[0].y == this.snakeTrail[i].y) {
+
+                this.velX = this.velY = 0;
+
+                this.gameBack.lose = true;
+                this.gameBack.oldScore = this.gameBack.score;
+                this.gameBack.score = 0;
+                this.clearSnake();
+
+                document.getElementById('restart').style.display = 'block';
+
+                console.log("YOU LOSE!");
+
+                return true;
+            } else {
+                //nada
+            }
+        }
+        return false;
+    }
+
+    draw() {
+        this.moveSnake();
+        this.setTrail();
+        this.lose();
+
+        for (let i = 0; i < this.snakeTrail.length; i++) {
+            if (i == 0) {
+                this.gameContext.fillStyle = '#e67e22';
+            } else {
+                this.gameContext.fillStyle = '#6be63e';
+            }
+            this.gameContext.fillRect(this.snakeTrail[i].x * this.gameDiv,
+                this.snakeTrail[i].y * this.gameDiv, this.gameDiv - 2, this.gameDiv - 2);
+        }
+    }
+
+    setVel(theKeyCode) {
+
+        let setXY = (x, y) => {
+            this.velX = x;
+            this.velY = y;
+        }
+        if (!this.gameBack.lose) {
+
+            if ((theKeyCode == 37 || theKeyCode == 65) && this.permision) {//left
+                setXY(-this.vel, 0);
+                this.permision = !this.permision;
+            } else if ((theKeyCode == 38 || theKeyCode == 87) && !this.permision) {//up
+                setXY(0, -this.vel);
+                this.permision = !this.permision;
+            } else if ((theKeyCode == 39 || theKeyCode == 68) && this.permision) {//right
+                setXY(this.vel, 0);
+                this.permision = !this.permision;
+            } else if ((theKeyCode == 40 || theKeyCode == 83) && !this.permision) {//down
+                setXY(0, this.vel);
+                this.permision = !this.permision;
+            } else {
+                //none
+            }
+
+        } else if (theKeyCode == 32) {
+            this.gameBack.lose = false;
+            this.velX = this.vel;
+            this.permision = false;
+            document.getElementById('restart').style.display = 'none';
+        }
+
+    }
+
+    moveSnake() {
+        this.snakeX += this.velX;
+        this.snakeY += this.velY;
+
+        if (this.snakeX > 19) {
+            this.snakeX = 0;
+        } else if (this.snakeX < 0) {
+            this.snakeX = 19;
+        }
+
+        if (this.snakeY > 19) {
+            this.snakeY = 0;
+        } else if (this.snakeY < 0) {
+            this.snakeY = 19;
+        }
+    }
+
+}
+
+class createApple {
+    constructor(snake, gameContext, gameDiv, gameBack) {
+        this.snake = snake;
+        this.gameContext = gameContext;
+        this.gameDiv = gameDiv;
+        this.gameBack = gameBack;
+        this.appleX = 15;
+        this.appleY = 15;
+    }
+
+    gotEat() {
+        if (this.appleX == this.snake.snakeTrail[0].x &&
+            this.appleY == this.snake.snakeTrail[0].y) {
+
+            this.snake.snakeTrail.push({ "x": '', "y": '' });
+
+            this.gameBack.score += 10;
+
+            this.appleX = Math.floor(Math.random() * 20);
+            this.appleY = Math.floor(Math.random() * 20);
+        } else {
+            //nada
+        }
+    }
+
+    draw() {
+        this.gotEat();
+        this.gameContext.fillStyle = '#a31708';
+        this.gameContext.beginPath();
+        this.gameContext.arc((this.appleX * this.gameDiv) + (this.gameDiv / 2),
+            (this.appleY * this.gameDiv) + (this.gameDiv / 2), this.gameDiv / 2.5, 0, 2 * Math.PI);
+        this.gameContext.fill();
+    }
+}
+
+class createGameBack {
+    constructor(gameContext, gameWindow) {
+        this.gameContext = gameContext;
+        this.gameWindow = gameWindow;
+        this.score = 0;
+        this.lose = false;
+        this.oldScore = 0;
+    }
+
+    draw() {
+        this.gameContext.fillStyle = '#363787';
+        this.gameContext.fillRect(0, 0, this.gameWindow.width, this.gameWindow.height);
+
+        this.gameContext.fillStyle = '#fff';
+        this.gameContext.font = "14px Arial";
+        this.gameContext.fillText(this.score, 365, 20);
+
+        if (this.lose) {
+            this.gameContext.fillStyle = '#fff';
+            this.gameContext.font = "30px Arial";
+            this.gameContext.fillText("Game Over", 120, 150);
+
+            this.gameContext.fillStyle = '#fff';
+            this.gameContext.font = "20px Arial";
+            this.gameContext.fillText(`Your Score: ${this.oldScore}`, 130, 180);
+
+            this.gameContext.fillStyle = '#fff';
+            this.gameContext.font = "15px Arial";
+            this.gameContext.fillText('Jogar de novo? Aperte espaço!', 100, 250);
+        }
+    }
+}
+
+function createCanvas(app, width, height) {
+    const canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    app.appendChild(canvas);
+    return canvas;
+}
+
+window.onload = () => {
+
+    const app = document.getElementById('root');
+    const gameWindow = createCanvas(app, 400, 400);
+    const gameContext = gameWindow.getContext('2d');
+    const gameDiv = 20;
+
+    const gameBack = new createGameBack(gameContext, gameWindow);
+    const snake = new createSnake(gameContext, gameDiv, gameBack);
+    const apple = new createApple(snake, gameContext, gameDiv, gameBack);
+
+  
+    setInterval(function () {
+        gameBack.draw();
+        snake.draw();
+        apple.draw();
+
+    }, 1000 / 13);
+
+    //Controls
+    document.addEventListener('keydown', () => {
+        console.log(event.keyCode);
+        snake.setVel(event.keyCode);
+    });
+
 }
